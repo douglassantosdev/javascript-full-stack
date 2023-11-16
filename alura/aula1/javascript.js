e75e61f3d5914105d8235d008e572ec1092b9aa4
Douglassantosdev <dougssdev@gmail.com>
2023-11-16 19:30:00 -0300

    EX

--- alura/aula1/javascript.js
+++ alura/aula1/javascript.js
@@ -2,9 +2,12 @@ var n1 = document.getElementById('n1')
 var cotacaoDoDolar = 4.87
 
 function converter() {
-    if(n1.value > 0) {
+    if (n1.value > 0) {
         mostrar.innerHTML = `R$ ${n1.value * cotacaoDoDolar}`
     } else {
         alert('Digite um numero maior que 0.')
     }
+    n1.focus();
+    n1.value = "";
 }
+
