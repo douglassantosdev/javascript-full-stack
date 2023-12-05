a561ff83e277c29928195763beac6e8158403970
Douglas Santos da Silva Junior <dougssdev@gmail.com>
2023-12-05 19:57:48 -0300

    Update javascript.js

--- modulo 6/ex001/javascript.js
+++ modulo 6/ex001/javascript.js
@@ -1,1 +1,8 @@
-alert("meu codigo js externo")
+let namePersonOlder = prompt("Qual o nome da pessoa mais velha?")
+
+let agePersonOlder = prompt("Qual idade da pessoa mais velha?")
+
+let namePersonYonger = prompt("Qual o nome da pessoa mais nova?")
+
+let agePersonYonger = prompt("Qual a idade da pessoa mais nova?")
+
