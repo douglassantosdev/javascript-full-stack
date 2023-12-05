eacfe266e7956b5fc1d0de244a41b61a99854ddd
Douglas Santos da Silva Junior <dougssdev@gmail.com>
2023-12-05 20:08:36 -0300

    Update javascript.js

--- modulo 6/ex001/javascript.js
+++ modulo 6/ex001/javascript.js
@@ -6,3 +6,10 @@ let namePersonYonger = prompt("Qual o nome da pessoa mais nova?")
 
 let agePersonYonger = prompt("Qual a idade da pessoa mais nova?")
 
+alert(`A pessoa mais velha tem ${agePersonOlder} de idade, e a pessoa mais nova tem ${agePersonYonger}.`)
+
+let personOlder = alert(`Nome da pessoa mais velha: ${namePersonOlder} \nIdade da pessoa mais velha: ${agePersonOlder}`)
+
+let personYonger = alert(`Nome da pessoa mais nova: ${namePersonYonger} \nIdade da pessoa mais nova: ${agePersonYonger}`)
+
+let ageDif = alert(`A diferença de idade entre elas é de ${agePersonOlder - agePersonYonger} anos`)
